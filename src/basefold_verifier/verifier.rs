f130d219f496fce1d8ea82ad674b8702f277ef33
Yuncong Zhang <yczhangsjtu@gmail.com>
2025-07-16 10:21:23 +0800

    Check query opening proof len

--- src/basefold_verifier/verifier.rs
+++ src/basefold_verifier/verifier.rs
@@ -24,7 +24,11 @@ pub(crate) fn batch_verifier<C: Config + Debug>(
     let expected_final_message_size: Var<C::N> = builder.eval(Usize::<C::N>::from(1usize)); // TODO: support early stop?
     iter_zip!(builder, proof.final_message).for_each(|ptr_vec, builder| {
         let final_message_len = builder.iter_ptr_get(&proof.final_message, ptr_vec[0]).len();
-        let final_message_len: Var<C::N> = builder.eval(final_message_len);
-        builder.assert_eq::<Usize<C::N>>(final_message_len, expected_final_message_size);
+        builder.assert_eq::<Var<C::N>>(final_message_len, expected_final_message_size);
     });
+
+    builder.assert_nonzero(&proof.sumcheck_proof.len());
+    // TODO: get this number from some config instead of hardcoding
+    let expected_num_queries: Var<C::N> = builder.eval(Usize::<C::N>::from(100usize));
+    builder.assert_eq::<Var<C::N>>(proof.query_opening_proof.len(), expected_num_queries);
 }
