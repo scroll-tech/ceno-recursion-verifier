af9e8a006b1249c0ce56d7e57fcc0d5dcf268291
Ray Gao <qg2153@columbia.edu>
2025-10-13 18:03:50 -0400

    Recover verifier logic

--- src/zkvm_verifier/verifier.rs
+++ src/zkvm_verifier/verifier.rs
@@ -358,7 +358,6 @@ pub fn verify_zkvm_proof<C: Config<F = F>>(
         });
     }
 
-    /* _debug
     builder.assert_usize_eq(num_chips_have_fixed, Usize::from(num_fixed_opening));
     builder.assert_eq::<Usize<_>>(num_chips_verified, chip_indices.len());
 
@@ -434,7 +433,6 @@ pub fn verify_zkvm_proof<C: Config<F = F>>(
     // logup check
     let zero: Ext<C::F, C::EF> = builder.constant(C::EF::ZERO);
     builder.assert_ext_eq(logup_sum, zero);
-    */
 }
 
 pub fn verify_opcode_proof<C: Config>(
