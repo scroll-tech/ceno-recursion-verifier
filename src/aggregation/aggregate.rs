afae2a6f63250c854c96bb29606e590635fdd891
Ray Gao <qg2153@columbia.edu>
2025-10-13 20:43:16 -0400

    Export proof:

--- src/aggregation/aggregate.rs
+++ src/aggregation/aggregate.rs
@@ -16,8 +16,10 @@ use openvm_continuations::verifier::{
     leaf::types::LeafVmVerifierInput,
     root::types::RootVmVerifierInput,
 };
+use std::fs::File;
+use std::io::Write;
 use openvm_native_recursion::hints::Hintable;
-const NUM_CHILDREN: usize = 1;
+const NUM_CHILDREN: usize = 2;  // _debug: param
 const NUM_CHILDREN_INTERNAL: usize = 2;
 use std::time::Instant;
 
@@ -30,6 +32,11 @@ pub fn compress_to_root_proof(
     let segmented_continuation_proof = stark_prover.app_prover.generate_app_proof(witness_stream.into());
     println!("Aggreation - Generated segemented (count: {:?}) continuation proof at: {:?}", segmented_continuation_proof.per_segment.len(), aggregation_start_timestamp.elapsed());
 
+    // _debug: export
+    let json = serde_json::to_string_pretty(&segmented_continuation_proof).unwrap();
+    let mut file = File::create("segmented_continuation_proof.json").expect("Create export proof file");
+    file.write_all(json.as_bytes()).expect("Export proof");
+
     let public_values = segmented_continuation_proof.user_public_values.public_values.clone();
     let leaf_inputs = LeafVmVerifierInput::chunk_continuation_vm_proof(&segmented_continuation_proof, NUM_CHILDREN);
 
@@ -39,8 +46,14 @@ pub fn compress_to_root_proof(
             SingleSegmentVmProver::prove(&leaf_prover, input.write_to_stream())
         })
         .collect::<Vec<_>>();
-
     println!("Aggregation - Generated {:?} leaf proofs at: {:?}", leaf_proofs.len(), aggregation_start_timestamp.elapsed());
+
+    // _debug: export
+    leaf_proofs.iter().enumerate().for_each(|(idx, p)| {
+        let json = serde_json::to_string_pretty(p).unwrap();
+        let mut file = File::create(format!("leaf_proof_{:?}.json", idx)).expect("Create export proof file");
+        file.write_all(json.as_bytes()).expect("Export proof");
+    });
 
     // Aggregate tree to root proof
     let internal_prover = stark_prover.agg_prover.internal_prover;
@@ -57,7 +70,7 @@ pub fn compress_to_root_proof(
                 .get_program_commit()
                 .into(),
             &proofs,
-            NUM_CHILDREN_INTERNAL,
+            stark_prover.agg_prover.num_children_internal,
         );
         proofs = internal_inputs
             .into_iter()
@@ -65,6 +78,12 @@ pub fn compress_to_root_proof(
                 internal_node_idx += 1;
                 let internal_proof = SingleSegmentVmProver::prove(&internal_prover, input.write());
                 println!("Aggregation - Completed internal node (idx: {:?}) at height {:?}: {:?}", internal_node_idx, internal_node_height, aggregation_start_timestamp.elapsed());
+
+                // _debug: export
+                let json = serde_json::to_string_pretty(&internal_proof).unwrap();
+                let mut file = File::create(format!("internal_proof_{:?}_height_{:?}.json", internal_node_idx, internal_node_height)).expect("Create export proof file");
+                file.write_all(json.as_bytes()).expect("Export proof");
+
                 internal_proof
             })
             .collect();
@@ -76,6 +95,11 @@ pub fn compress_to_root_proof(
         proof: proofs.pop().unwrap(),
         user_public_values: public_values,
     };
+
+    // _debug: export
+    let json = serde_json::to_string_pretty(&root_stark_proof).unwrap();
+    let mut file = File::create("root_proof_with_public_values.json").expect("Create export proof file");
+    file.write_all(json.as_bytes()).expect("Export proof");
 
     println!("Aggregation - Completed root proof: {:?}", aggregation_start_timestamp.elapsed());
 
